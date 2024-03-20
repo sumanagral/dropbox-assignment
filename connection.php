95ff4650ada776e24c7c10168604cfcbdd05fc52
sumanagral <83566582+sumanagral@users.noreply.github.com>
2024-03-19 20:14:16 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 /* Database connection settings */
-$host = 'dropbox-assignment.c5v9qtdaz2yk.us-east-1.rds.amazonaws.com';
-$user = 'dropboxasg';
-$pass = 'injection123';
-$db = 's3web';
+$host = '';
+$user = '';
+$pass = '';
+$db = '';
 $mysqli = new mysqli($host, $user, $pass, $db, "3306") or die($mysqli->error);
